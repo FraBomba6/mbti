963b51b0070fd5131a589158b16bca3c83df17c7
lara.schober <lara.schober@student.kit.edu>
2023-02-04 14:12:33 +0100

    tried to set it up somehow

--- src/classificationXLNet.py
+++ src/classificationXLNet.py
@@ -1,5 +1,41 @@
 #importing XLNet
 from transformers import XLNetTokenizer, XLNetModel
+from simpletransformers.classification import ClassificationModel
+import pandas as pd
+import logging
+import sklearn
+import torch.nn as nn
+import preprocessing as prep
+import pandas as pd
+import re
+import os
+import math
+import torch
+# import torch.nn as nn
+from torch.nn import BCEWithLogitsLoss, NLLLoss
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+# from pytorch_transformers import XLNetModel, XLNetTokenizer, XLNetForSequenceClassification
+from transformers import AdamW, XLNetTokenizer, XLNetModel, TFXLNetModel, XLNetLMHeadModel, XLNetConfig, XLNetForSequenceClassification
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tqdm import tqdm, trange
+import matplotlib.pyplot as plt
+import unicodedata
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix,roc_auc_score
+import seaborn as sns
+import itertools
+import plotly
+import seaborn as sns
+from plotly import graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+from collections import Counter
+
+
+#df = pd.read_csv('dataset.csv')
+#train_df=pd.DataFrame('dataset.csv')
+#test_df=pd.DataFrame('dataset.csv')
 
 tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
 model = XLNetModel.from_pretrained('xlnet-base-cased')
@@ -43,21 +79,28 @@ train_args = {
 
 #%%
 #Training the model
-from simpletransformers.classification import ClassificationModel
-import pandas as pd
-import logging
-import sklearn
+
 
 logging.basicConfig(level=logging.DEBUG)
 transformers_logger = logging.getLogger('transformers')
 transformers_logger.setLevel(logging.WARNING)
 
 # We use the XLNet base cased pre-trained model.
-model = ClassificationModel('xlnet', 'xlnet-base-cased', num_labels=2, args=train_args)
+#model = ClassificationModel('xlnet', 'xlnet-base-cased', num_labels=2, args=train_args)
 
 # Train the model, there is no development or validation set for this dataset
 # https://simpletransformers.ai/docs/tips-and-tricks/#using-early-stopping
-model.train_model(train_df)
+#model.train_model(train_df)
 
 # Evaluate the model in terms of accuracy score
-result, model_outputs, wrong_predictions = model.eval_model(test_df, acc=sklearn.metrics.accuracy_score)
+#result, model_outputs, wrong_predictions = model.eval_model(test_df, acc=sklearn.metrics.accuracy_score)
+
+class XLNet_Model(nn.Module):
+  def __init__(self, classes):
+    super(XLNet_Model, self).__init__()
+    self.xlnet = XLNetModel.from_pretrained('xlnet-base-cased')
+    self.out = nn.Linear(self.xlnet.config.hidden_size, classes)
+  def forward(self, input):
+    outputs = self.xlnet(**input)
+    out = self.out(outputs.last_hidden_state)
+    return out
