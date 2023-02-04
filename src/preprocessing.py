8c58af416dd8a3c33b8c3c64e6b543657ef5580a
uyhdx <uyhdx@student.kit.edu>
2023-02-04 12:44:23 +0100

    changed 'frown', version before Lara changes smiley faces

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,13 +1,7 @@
 #import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import re
-import nltk
 # from transformers import BertForSequenceClassification, BertTokenizer
-
-# FUNCTIONS
-def remove_urls(text):
-    return re.sub(r'http\S+', '', text)
 
 # %%
 # IF YOU RUN USING CHUNKS
@@ -16,7 +10,6 @@ print(mbti_dataset.head(5))
 # ELSE
 #mbti_dataset = pd.read_csv("../data/dataset.csv")
 
-#print(mbti_dataset)
 #TODO: decide how to hand the words/sentences to the model: 1 sentence, 2 sentences, half-half,
 # tokenization: tokener will split sentence
 
@@ -28,10 +21,9 @@ words_to_remove = [r'http\S+', '|', '_', 'infp', 'infj', 'intp', 'intj', 'entp',
 for word in words_to_remove:
     mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(word, '', regex=True)
 
-
 # %% CONVERT SMILEY FACES INTO WORDS
 mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(r":-\)|:\)", 'smile', regex=True)
-mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(r":-\)|:\)", 'frown', regex=True)
+mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(r":-\(|:\(", 'frown', regex=True)
 mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(r";-\)|;\)", 'wink', regex=True)
 mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(r":-D|:D", 'grin', regex=True)
 mbti_dataset['posts'] = mbti_dataset['posts'].str.replace(r":-P|:P", 'tongue_out', regex=True)
