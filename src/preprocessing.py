819bc72dc35d75ab14ab3bc56174f8c851024fa8
lara.schober <lara.schober@student.kit.edu>
2023-02-08 11:52:00 +0100

    some changes to stack the torches

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+import numpy as np
 
 # %%
 # IF YOU RUN USING CHUNKS
@@ -47,28 +48,37 @@ new_column_order = ['type', 'I-E', 'N-S', 'T-F', 'J-P', 'posts']
 mbti_dataset = mbti_dataset.reindex(columns=new_column_order)
 
 # %% Encode Type letters to 0 and 1
-mbti_dataset['I-E'] = mbti_dataset['I-E'].str.replace("I", '0')
-mbti_dataset['I-E'] = mbti_dataset['I-E'].str.replace("E", '1')
-mbti_dataset['N-S'] = mbti_dataset['N-S'].str.replace("N", '0')
-mbti_dataset['N-S'] = mbti_dataset['N-S'].str.replace("S", '1')
-mbti_dataset['T-F'] = mbti_dataset['T-F'].str.replace("T", '0')
-mbti_dataset['T-F'] = mbti_dataset['T-F'].str.replace("F", '1')
-mbti_dataset['J-P'] = mbti_dataset['J-P'].str.replace("J", '0')
-mbti_dataset['J-P'] = mbti_dataset['J-P'].str.replace("P", '1')
+# mbti_dataset['I-E'] = mbti_dataset['I-E'].str.replace("I", '0')
+# mbti_dataset['I-E'] = mbti_dataset['I-E'].str.replace("E", '1')
+# mbti_dataset['N-S'] = mbti_dataset['N-S'].str.replace("N", '0')
+# mbti_dataset['N-S'] = mbti_dataset['N-S'].str.replace("S", '1')
+# mbti_dataset['T-F'] = mbti_dataset['T-F'].str.replace("T", '0')
+# mbti_dataset['T-F'] = mbti_dataset['T-F'].str.replace("F", '1')
+# mbti_dataset['J-P'] = mbti_dataset['J-P'].str.replace("J", '0')
+# mbti_dataset['J-P'] = mbti_dataset['J-P'].str.replace("P", '1')
+
+mbti_dataset['I-E'] = mbti_dataset['I-E'].apply(lambda x: torch.from_numpy(np.array([1, 0])) if x == 'I' else torch.from_numpy(np.array([0, 1])))
+mbti_dataset['N-S'] = mbti_dataset['N-S'].apply(lambda x: torch.from_numpy(np.array([1, 0])) if x == 'N' else torch.from_numpy(np.array([0, 1])))
+mbti_dataset['T-F'] = mbti_dataset['T-F'].apply(lambda x: torch.from_numpy(np.array([1, 0])) if x == 'T' else torch.from_numpy(np.array([0, 1])))
+mbti_dataset['J-P'] = mbti_dataset['J-P'].apply(lambda x: torch.from_numpy(np.array([1, 0])) if x == 'J' else torch.from_numpy(np.array([0, 1])))
 
 # %%
 i_e = mbti_dataset['I-E'].tolist()
-i_e = [int(x) for x in i_e]
-i_e = torch.Tensor(i_e)
+# i_e = [int(x) for x in i_e]
+# i_e = torch.Tensor(i_e)
+i_e = torch.stack(i_e)
 n_s = mbti_dataset['N-S'].tolist()
-n_s = [int(x) for x in n_s]
-n_s = torch.Tensor(n_s)
+#n_s = [int(x) for x in n_s]
+#n_s = torch.Tensor(n_s)
+n_s = torch.stack(i_e)
 t_f = mbti_dataset['T-F'].tolist()
-t_f = [int(x) for x in t_f]
-t_f = torch.Tensor(t_f)
+#t_f = [int(x) for x in t_f]
+#t_f = torch.Tensor(t_f)
+f_f = torch.stack(i_e)
 j_p = mbti_dataset['J-P'].tolist()
-j_p = [int(x) for x in j_p]
-j_p = torch.Tensor(j_p)
+#j_p = [int(x) for x in j_p]
+#j_p = torch.Tensor(j_p)
+j_p = torch.stack(i_e)
 
 # type_data = torch.stack((i_e, n_s, t_f, j_p), dim=1) #not needed
 # type_data = (i_e, n_s, t_f, j_p)
