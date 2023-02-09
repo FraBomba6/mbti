39733ce5d7df00d279c12f85d693aad40daf1c61
bomb697 <50354770+bomb697@users.noreply.github.com>
2023-02-09 11:07:00 +0100

    feat: roberta corrections

--- src/classifier.py
+++ src/classifier.py
@@ -9,8 +9,7 @@ def get_model(model_string: str):
     if model_string == "xlnet":
         model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased", num_labels=2)
     elif model_string == "roberta":
-        model = RobertaForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-emotion", num_labels=2)
-        model.classifier = torch.nn.Linear(768, 2)
+        model = RobertaForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-emotion", num_labels=2, ignore_mismatched_sizes=True)
     else:
         raise Exception("The model is neither XLNet nor RoBERTa. Valid arguments: xlnet, roberta")
     return model
