db55d88eca928fea983b2fc5434f32935f87a428
bomb697 <50354770+bomb697@users.noreply.github.com>
2023-02-09 11:04:34 +0100

    feat: roberta corrections and requirements

--- src/classifier.py
+++ src/classifier.py
@@ -1,4 +1,5 @@
 from transformers import XLNetForSequenceClassification, RobertaForSequenceClassification
+import torch
 from rich.console import Console
 rich = Console()
 
@@ -9,6 +10,7 @@ def get_model(model_string: str):
         model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased", num_labels=2)
     elif model_string == "roberta":
         model = RobertaForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-emotion", num_labels=2)
+        model.classifier = torch.nn.Linear(768, 2)
     else:
         raise Exception("The model is neither XLNet nor RoBERTa. Valid arguments: xlnet, roberta")
     return model
